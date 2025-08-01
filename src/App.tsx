f2754e7edbc07f9df35684aa15df5227d9e1562e
chen yang <wszgrcy@gmail.com>
2025-08-01 16:24:54 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import './App.css';
 import { PiyingPage } from './piying-page';
-import '@valibot/i18n/zh-CN'
-import { setGlobalConfig } from 'valibot'
-const browserLanguage = navigator.language
-if (browserLanguage.startsWith('zh')) {
-  setGlobalConfig({ lang: 'zh-CN' })
-}
+
 function App() {
   return (
     <>
